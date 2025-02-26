167cd65f6f3a6cfce68d5199ed7a30230f5d4fea
emilio-espinosa <emiespeli@alum.us.es>
2025-02-26 17:35:36 +0100

    feat: created Review entity

---
+++ src/main/java/acme/entities/Review.java
@@ -0,0 +1,73 @@
+/*
+ * Review.java
+ *
+ * Copyright (C) 2012-2025 G3-C1.046
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Moment;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidScore;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(indexes = {
+	@Index(columnList = "id")
+})
+public class Review extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(min = 1, max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidMoment
+	@Automapped
+	private Moment				moment;
+
+	@Mandatory
+	@ValidString(min = 1, max = 50)
+	@Automapped
+	private String				subject;
+
+	@Mandatory
+	@ValidString(min = 1)
+	@Automapped
+	private String				text;
+
+	@Optional
+	@ValidScore
+	@Automapped
+	private Integer				score;
+
+	@Mandatory
+	@Automapped
+	private Boolean				recommended;
+
+}
