eaa1cd81d0bf89a068053ad42227bf98fa88fa9a
rafseggom <rafseggom@alum.us.es>
2025-03-03 23:40:50 +0100

    fix(aircraft): added index for table

--- src/main/java/acme/entities/aircrafts/Aircraft.java
+++ src/main/java/acme/entities/aircrafts/Aircraft.java
@@ -3,6 +3,8 @@ package acme.entities.aircrafts;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
@@ -23,7 +25,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-
+@Table(indexes = {
+	@Index(columnList = "id")
+})
 public class Aircraft extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
