8c69e3065f6a79808703954820f02a4fb1608d4f
rafseggom <rafseggom@alum.us.es>
2025-03-03 23:53:24 +0100

    feat(manager): added atributes

--- src/main/java/acme/entities/airlines/Manager.java
+++ src/main/java/acme/entities/airlines/Manager.java
@@ -1,6 +1,34 @@
+
 package acme.entities.airlines;
 
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
 
-public class Manager {
+public class Manager extends AbstractEntity {
 
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Automapped
+	private String				identifierNumber;
+
+	@Mandatory
+	@ValidNumber(max = 50)  //TODO: Cambiar a derivada? Max < birth-currentYear
+	@Automapped
+	private Integer				yearsOfExperience;
+
+	@Optional
+	@ValidUrl
+	@Automapped
+	private String				linkToPicture;
 }
