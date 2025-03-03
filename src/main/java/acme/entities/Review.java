4d54e28e11d26552e39d4c671ac8807bb1a00fe1
emilio-espinosa <emiespeli@alum.us.es>
2025-03-03 18:55:11 +0100

    Updated score in review.java so its matches the criteria needed

--- src/main/java/acme/entities/Review.java
+++ src/main/java/acme/entities/Review.java
@@ -22,7 +22,7 @@ import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
-import acme.client.components.validation.ValidScore;
+import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
 import lombok.Getter;
 import lombok.Setter;
@@ -62,7 +62,7 @@ public class Review extends AbstractEntity {
 	private String				text;
 
 	@Optional
-	@ValidScore
+	@ValidNumber(min = 0, max = 10, integer = 2, fraction = 2)
 	@Automapped
 	private Integer				score;
 
