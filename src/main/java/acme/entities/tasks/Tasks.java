20cf03f5fcded596ad1d292b79789ad2957ee069
emilio-espinosa <emiespeli@alum.us.es>
2025-03-05 11:48:48 +0100

    feature(tasks) added relations between tasks and MR and technicians

--- src/main/java/acme/entities/tasks/Tasks.java
+++ src/main/java/acme/entities/tasks/Tasks.java
@@ -2,12 +2,16 @@
 package acme.entities.tasks;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.entities.maintenaceRecords.MaintenanceRecords;
+import acme.entities.technicians.Technicians;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,6 +25,15 @@ public class Tasks extends AbstractEntity {
 	private static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Technicians			technician;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private MaintenanceRecords	maintenanceRecords;
 
 	@Mandatory
 	@Automapped
