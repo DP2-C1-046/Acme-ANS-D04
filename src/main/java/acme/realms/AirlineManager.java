529c5e240b634fa134bccc94de0b4828dfc14435
rafseggom <rafseggom@alum.us.es>
2025-03-10 01:52:02 +0100

    feat: added custom validator, constraints, repository and derived attributes

--- src/main/java/acme/realms/AirlineManager.java
+++ src/main/java/acme/realms/AirlineManager.java
@@ -4,8 +4,11 @@ package acme.realms;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
 import acme.client.components.mappings.Automapped;
@@ -15,7 +18,15 @@ import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidAirlineManager;
+import acme.entities.airlines.Airline;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@ValidAirlineManager
 public class AirlineManager extends AbstractRole {
 
 	// Serialisation version --------------------------------------------------
@@ -30,17 +41,26 @@ public class AirlineManager extends AbstractRole {
 	private String				identifierNumber;
 
 	@Mandatory
-	@ValidNumber(min = 0, integer = 2, fraction = 0, max = 50)  //TODO: Cambiar a derivada? Max < birth-currentYear
+	@ValidNumber(min = 0, integer = 3, fraction = 0, max = 120)  //TODO: Cambiar a derivada? Max < birth-currentYear
 	@Automapped
 	private Integer				yearsOfExperience;
 
 	@Mandatory
 	@ValidMoment(past = true)
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date				dateOfBirth;
+	private Date				birthDate;
 
 	@Optional
 	@ValidUrl
 	@Automapped
 	private String				linkToPicture;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airline				airline;
+
 }
