276ddbd0fb530fb674ecf2c702db13ec3bb82edf
emilio-espinosa <emiespeli@alum.us.es>
2025-03-11 19:02:11 +0100

    Feature(Technicians) Technicians relations added

--- src/main/java/acme/entities/technicians/Technicians.java
+++ src/main/java/acme/entities/technicians/Technicians.java
@@ -3,7 +3,9 @@ package acme.entities.technicians;
 
 import javax.persistence.Entity;
 import javax.persistence.Index;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
 import acme.client.components.mappings.Automapped;
@@ -11,6 +13,7 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.entities.aircrafts.Aircraft;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,6 +30,11 @@ public class Technicians extends AbstractRole {
 	private static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Aircraft			aircraft;
+
 	@Mandatory
 	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
 	@Automapped
