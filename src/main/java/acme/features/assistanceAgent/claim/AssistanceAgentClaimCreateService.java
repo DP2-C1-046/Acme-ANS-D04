3aaff82cba901d5cd7c7500831598a40d4519f87
Nerea Jiménez <nerjimado@alum.us.es>
2025-05-10 14:41:18 +0200

    hola

---
+++ src/main/java/acme/features/assistanceAgent/claim/AssistanceAgentClaimCreateService.java
@@ -0,0 +1,91 @@
+
+package acme.features.assistanceAgent.claim;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.views.SelectChoices;
+import acme.client.helpers.MomentHelper;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.claims.Claim;
+import acme.entities.claims.ClaimType;
+import acme.entities.legs.Leg;
+import acme.realms.AssistanceAgent;
+
+@GuiService
+public class AssistanceAgentClaimCreateService extends AbstractGuiService<AssistanceAgent, Claim> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AssistanceAgentClaimRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Claim claim;
+		Date registrationMoment;
+		AssistanceAgent assistanceAgent = (AssistanceAgent) super.getRequest().getPrincipal().getActiveRealm();
+
+		registrationMoment = MomentHelper.getCurrentMoment();
+
+		claim = new Claim();
+		claim.setRegistrationMoment(registrationMoment);
+		claim.setPassengerEmail("");
+		claim.setDescription("");
+		claim.setAssistanceAgent(assistanceAgent);
+		claim.setClaimType(ClaimType.FLIGHT_ISSUES);
+
+		super.getBuffer().addData(claim);
+	}
+
+	@Override
+	public void bind(final Claim claim) {
+		super.bindObject(claim, "registrationMoment", "passengerEmail", "description", "type", "leg");
+	}
+
+	@Override
+	public void validate(final Claim claim) {
+		;
+	}
+
+	@Override
+	public void perform(final Claim claim) {
+		Date registrationMoment;
+
+		registrationMoment = MomentHelper.getCurrentMoment();
+		claim.setRegistrationMoment(registrationMoment);
+		this.repository.save(claim);
+	}
+
+	@Override
+	public void unbind(final Claim claim) {
+		Collection<Leg> legs;
+		SelectChoices choices;
+		SelectChoices choices2;
+		Dataset dataset;
+
+		choices = SelectChoices.from(ClaimType.class, claim.getClaimType());
+		legs = this.repository.findAllLeg();
+		choices2 = SelectChoices.from(legs, "flightNumber", claim.getLeg());
+
+		dataset = super.unbindObject(claim, "registrationMoment", "passengerEmail", "description", "type", "leg");
+		dataset.put("readonly", false);
+		dataset.put("types", choices);
+		dataset.put("legs", choices2);
+
+		super.getResponse().addData(dataset);
+	}
+
+}
